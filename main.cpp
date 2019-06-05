f4b16dd77d673b3b990f815526511f3b57724bec
uc0de <cristidragomir97@gmail.com>
2019-06-05 17:36:35 +0200

    unfucked

--- main.cpp
+++ main.cpp
@@ -22,8 +22,7 @@ using namespace cv;
 using namespace rs2;
 using namespace std;
 
-Scalar get_colour(double v, double vmin, double vmax)
-{
+Scalar get_colour(double v, double vmin, double vmax){
     double r, g, b = 0;
     double dv;
 
@@ -47,28 +46,33 @@ Scalar get_colour(double v, double vmin, double vmax)
     return cv::Scalar(b * 255, g * 255, r * 255);
 }
 
-void average_depth(rs2::depth_frame frame, Mat tmp, bool display) {
+void process_frame(rs2::depth_frame this_frame, int slices, int vibrators, int bins, const char * window, bool display) {
+
+    Mat raw, tmp, out;
+    colorizer color_map;
+
+    int h_step = HEIGHT / slices;
+    int w_step = WIDTH / vibrators;
+    int d_step = MAX_DISTANCE / bins;
+
     rs2::decimation_filter dec;
     rs2::spatial_filter spat;
 
     dec.set_option(RS2_OPTION_FILTER_MAGNITUDE, 2);
-    spat.set_option(RS2_OPTION_HOLES_FILL, thereshold);
+    spat.set_option(RS2_OPTION_HOLES_FILL, 4);
 
-    frame.apply_filter(dec).apply_filter(spat);
+    this_frame.apply_filter(dec).apply_filter(spat);
 
 
     for (int w = 0; w < vibrators; w++) {
         for (int h = 0; h < slices; h++) {
-
             long p_sum = 1;
             long v_sum = 0;
-
-            char buffer[5];
 
             for (int j = h * h_step; j < (h + 1) * h_step; j+=1) {
                 for (int i = w * w_step; i < (w + 1) * w_step; i+=1) {
 
-                    long this_val = long(frame.get_distance(i, j) * 1000);
+                    long this_val = long(this_frame.get_distance(i, j) * 1000);
                     if(i > 20 && j > 20){
                         for (int k = 0; k < bins; k++){
                             if (this_val > d_step * k && this_val < d_step * (k + 1)){
@@ -84,11 +88,24 @@ void average_depth(rs2::depth_frame frame, Mat tmp, bool display) {
             long avg = v_sum / p_sum;
 
             if(display){
-                sprintf(buffer,"%d",avg);
+                char buffer[5];
+                sprintf(buffer,"%ld", avg);
                 rectangle(tmp, cv::Point((w+1) * w_step,(h+1) * h_step),  cv::Point(w * w_step, h * h_step), get_colour(avg, 0, MAX_DISTANCE), -1);
+
+                frame colorized = this_frame.apply_filter(color_map);
+                raw = Mat(Size(WIDTH, HEIGHT), CV_8UC3, (void *) colorized.get_data(), Mat::AUTO_STEP);
+                tmp = Mat(Size(WIDTH, HEIGHT), CV_8UC3, Scalar(0, 0, 0));
+                out = Mat(Size(WIDTH, HEIGHT), CV_8UC3, Scalar(0, 0, 0));
+
+                // draw grid on input image
+                for (int i = 0; i < HEIGHT; i += h_step)    line(raw, Point(0, i), Point(WIDTH, i), Scalar(0, 255, 255));
+                for (int i = 0; i < WIDTH; i += w_step)     line(raw, Point(i, 0), Point(i, HEIGHT), Scalar(255, 0, 255));
+
+                Mat dst;
+                hconcat(raw, tmp, dst);
+                hconcat(dst, out, dst);
+                imshow(window, dst);
             }
-
-
 
           /// map_values(w, height_index, min_distance);
         }
@@ -123,7 +140,7 @@ int main(int argc, char * argv[]) try {
     pipeline pipe;
     config cfg;
     colorizer color_map;
-    Mat raw, tmp,  out;
+
 
     init_rs(&pipe, &cfg);
     auto window = init_gui(&vibrators, &slices, &bins);
@@ -131,36 +148,12 @@ int main(int argc, char * argv[]) try {
     //serial port = serial(PORT, method);
 
     while (waitKey(1) < 0 && getWindowProperty(window, WND_PROP_AUTOSIZE) >= 0) {
-
-        // recalculate at every frame, cuz it might change on runtime
-        h_step = HEIGHT / slices;
-        w_step = WIDTH / vibrators;
-        d_step = MAX_DISTANCE / bins;
-
         // poll for frames
         frameset data = pipe.wait_for_frames();
         frame depth = data.get_depth_frame();
 
-        if(display){
-            frame colorized = depth.apply_filter(color_map);
-            raw = Mat(Size(WIDTH, HEIGHT), CV_8UC3, (void *) colorized.get_data(), Mat::AUTO_STEP);
-            tmp = Mat(Size(WIDTH, HEIGHT), CV_8UC3, Scalar(0, 0, 0));
-            out = Mat(Size(WIDTH, HEIGHT), CV_8UC3, Scalar(0, 0, 0));
-
-            // draw grid on input image
-            for (int i = 0; i < HEIGHT; i += h_step)    line(raw, Point(0, i), Point(WIDTH, i), Scalar(0, 255, 255));
-            for (int i = 0; i < WIDTH; i += w_step)     line(raw, Point(i, 0), Point(i, HEIGHT), Scalar(255, 0, 255));
-        }
-
         // send it to the averaging layer
-        average_depth(depth, tmp, display);
-
-        if (display) {
-            Mat dst;
-            hconcat(raw, tmp, dst);
-            hconcat(dst, out, dst);
-            imshow(window, dst);
-        }
+        process_frame(depth, slices, vibrators, bins, window, display);
     }
     return EXIT_SUCCESS;
 
