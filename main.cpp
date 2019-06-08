8095f1a941dfce1193cc0a6576db0dfda316b5d7
uc0de <cristidragomir97@gmail.com>
2019-06-08 15:10:58 +0200

    kinda fucked it up, but not really

--- main.cpp
+++ main.cpp
@@ -12,17 +12,21 @@
 #include <opencv2/highgui/highgui.hpp>
 
 
-#define PORT "/dev/ttyUSB0"
-#define MAX_DISTANCE 10000
-#define BASE_RATE 320
+#define MAX_DISTANCE 8000
 #define WIDTH 640
 #define HEIGHT 480
 
-using namespace cv;
-using namespace rs2;
-using namespace std;
+int vibrators = 32;
+int slices = 10;
+int bins = 96;
 
-Scalar get_colour(double v, double vmin, double vmax){
+int h_step = 0;
+int w_step = 0;
+int d_step = 0;
+
+using namespace cv;
+
+cv::Scalar get_colour(double v, double vmin, double vmax){
     double r, g, b = 0;
     double dv;
 
@@ -46,23 +50,20 @@ Scalar get_colour(double v, double vmin, double vmax){
     return cv::Scalar(b * 255, g * 255, r * 255);
 }
 
-void process_frame(rs2::depth_frame this_frame, int slices, int vibrators, int bins, const char * window, bool display) {
 
-    Mat raw, tmp, out;
-    colorizer color_map;
-
-    int h_step = HEIGHT / slices;
-    int w_step = WIDTH / vibrators;
-    int d_step = MAX_DISTANCE / bins;
-
+void process_frame(rs2::depth_frame frame, cv::Mat image, cv::Mat out_image) {
     rs2::decimation_filter dec;
     rs2::spatial_filter spat;
 
     dec.set_option(RS2_OPTION_FILTER_MAGNITUDE, 2);
-    spat.set_option(RS2_OPTION_HOLES_FILL, 4);
+    spat.set_option(RS2_OPTION_HOLES_FILL, 5);
 
-    this_frame.apply_filter(dec).apply_filter(spat);
+    frame.apply_filter(dec).apply_filter(spat);
+    h_step = HEIGHT / slices;
+    w_step = WIDTH / vibrators;
+    d_step = MAX_DISTANCE / bins;
 
+    int l1_out[slices][vibrators];
 
     for (int w = 0; w < vibrators; w++) {
         for (int h = 0; h < slices; h++) {
@@ -72,7 +73,7 @@ void process_frame(rs2::depth_frame this_frame, int slices, int vibrators, int b
             for (int j = h * h_step; j < (h + 1) * h_step; j+=1) {
                 for (int i = w * w_step; i < (w + 1) * w_step; i+=1) {
 
-                    long this_val = long(this_frame.get_distance(i, j) * 1000);
+                    long this_val = long(frame.get_distance(i, j) * 1000);
                     if(i > 20 && j > 20){
                         for (int k = 0; k < bins; k++){
                             if (this_val > d_step * k && this_val < d_step * (k + 1)){
@@ -86,82 +87,92 @@ void process_frame(rs2::depth_frame this_frame, int slices, int vibrators, int b
 
 
             long avg = v_sum / p_sum;
-
-            if(display){
-                char buffer[5];
-                sprintf(buffer,"%ld", avg);
-                rectangle(tmp, cv::Point((w+1) * w_step,(h+1) * h_step),  cv::Point(w * w_step, h * h_step), get_colour(avg, 0, MAX_DISTANCE), -1);
-
-                frame colorized = this_frame.apply_filter(color_map);
-                raw = Mat(Size(WIDTH, HEIGHT), CV_8UC3, (void *) colorized.get_data(), Mat::AUTO_STEP);
-                tmp = Mat(Size(WIDTH, HEIGHT), CV_8UC3, Scalar(0, 0, 0));
-                out = Mat(Size(WIDTH, HEIGHT), CV_8UC3, Scalar(0, 0, 0));
-
-                // draw grid on input image
-                for (int i = 0; i < HEIGHT; i += h_step)    line(raw, Point(0, i), Point(WIDTH, i), Scalar(0, 255, 255));
-                for (int i = 0; i < WIDTH; i += w_step)     line(raw, Point(i, 0), Point(i, HEIGHT), Scalar(255, 0, 255));
-
-                Mat dst;
-                hconcat(raw, tmp, dst);
-                hconcat(dst, out, dst);
-                imshow(window, dst);
-            }
-
-          /// map_values(w, height_index, min_distance);
+           // l1_out[w][h] = avg;
+            char buffer[5];
+            sprintf(buffer,"%d", avg);
+            cv::rectangle(out_image, cv::Point((w+1) * w_step,(h+1) * h_step),  cv::Point(w * w_step, h * h_step), get_colour(avg, 0, 5000), -1);
         }
     }
 
+    for (int w = 0; w < vibrators; w++) {
+        for (int h = 0; h < slices; h++) {
+            std::cout <<  l1_out[w][h]  << " " ;
+        }
+        std::cout << "\n";
+    }
+
+    std::cout << "___________________________" << "\n";
 }
 
-void init_rs(pipeline * p, config * c){
-    c -> enable_stream(RS2_STREAM_DEPTH, WIDTH, HEIGHT, RS2_FORMAT_Z16, 90);
-    p -> start(c);
-}
 
-auto init_gui(int * vibrators, int * slices, int  * bins){
-    const auto w = "openeyes";
-    namedWindow(w, WINDOW_AUTOSIZE);
-    cv::createTrackbar("vibrators", w, vibrators, 50);
-    cv::createTrackbar("slices", w, slices, 50);
-    cv::createTrackbar("bins", w, bins, 2000);
-
-    return w;
-}
-
-int main(int argc, char * argv[]) try {
-
-    bool display = true;
-    bool serial = true;
-
-    int vibrators = 32;
-    int slices = 32;
-    int bins = 96;
-
-    pipeline pipe;
-    config cfg;
-    colorizer color_map;
-
-
-    init_rs(&pipe, &cfg);
-    auto window = init_gui(&vibrators, &slices, &bins);
+int main(int argc, char * argv[]) try
+{
 
     //serial port = serial(PORT, method);
+    //pport = &port;
+    //pport -> update(0, 4096, 100);
 
-    while (waitKey(1) < 0 && getWindowProperty(window, WND_PROP_AUTOSIZE) >= 0) {
-        // poll for frames
-        frameset data = pipe.wait_for_frames();
-        frame depth = data.get_depth_frame();
+    rs2::pipeline pipe;
+    rs2::config cfg;
+    rs2::colorizer color_map;
 
-        // send it to the averaging layer
-        process_frame(depth, slices, vibrators, bins, window, display);
+    cfg.enable_stream(RS2_STREAM_DEPTH, WIDTH, HEIGHT, RS2_FORMAT_Z16, 15);
+    pipe.start(cfg);
+
+
+    const auto window_name = "openeyez_core_v0";
+    const auto out_window = "openeyes_core out";
+    namedWindow(window_name, WINDOW_AUTOSIZE);
+    namedWindow(out_window, WINDOW_AUTOSIZE);
+
+
+    createTrackbar("vibrators", window_name, &vibrators, 50);
+    createTrackbar("slices", window_name, &slices, 50);
+    createTrackbar("bins", window_name, &bins, 300);
+
+
+
+
+    while (waitKey(1) < 0 && getWindowProperty(window_name, WND_PROP_AUTOSIZE) >= 0)
+    {
+        rs2::frameset data = pipe.wait_for_frames(); // Wait for next set of frames from the camera
+        rs2::frame depth = data.get_depth_frame();
+        rs2::frame colorized = depth.apply_filter(color_map);
+
+
+        // Query frame size (width and height)
+        const int w = depth.as<rs2::video_frame>().get_width();
+        const int h = depth.as<rs2::video_frame>().get_height();
+
+        // Create OpenCV matrix of size (w,h) from the colorized depth data
+        Mat image(Size(w, h), CV_8UC3, (void*)colorized.get_data(), Mat::AUTO_STEP);
+        Mat img(Size(w, h), CV_8UC3,Scalar(0,0,0));
+
+
+        process_frame(depth, image, img);
+
+        for(int i=0;i<h;i+=h_step)
+            cv::line(image, Point(0, i), Point(w, i), cv::Scalar(0, 255, 255));
+
+        for(int i=0;i<w;i+=w_step)
+            cv::line(image, Point(i, 0), Point(i, h), cv::Scalar(255, 0, 255));
+
+
+        // Update the window with new data
+        imshow(window_name, image);
+        imshow(out_window, img);
+
     }
-    return EXIT_SUCCESS;
 
-} catch (const rs2::error & e){
+    return EXIT_SUCCESS;
+}
+catch (const rs2::error & e)
+{
     std::cerr << "RealSense error calling " << e.get_failed_function() << "(" << e.get_failed_args() << "):\n    " << e.what() << std::endl;
     return EXIT_FAILURE;
 }
-catch (const std::exception& e){
+catch (const std::exception& e)
+{
     std::cerr << e.what() << std::endl;
     return EXIT_FAILURE;
 }
